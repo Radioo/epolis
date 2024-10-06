d3a55423fbc20467ed63fd0dfbec07758356f27c
KerWen <dominik.gumola@gmail.com>
2024-10-07 00:51:06 +0200

    add mask size slider and struct choice

--- src/epolis/text/dynamic_text.hpp
+++ src/epolis/text/dynamic_text.hpp
@@ -9,8 +9,13 @@ namespace epolis::text {
     public:
         void refresh_text();
         void add_button(wxButton* button);
+        void add_static_text(wxStaticText *text);
+
+        void add_choice(wxChoice *choice, const wxArrayString &values);
 
     private:
         std::unordered_map<wxButton*, std::string> buttons;
+        std::unordered_map<wxStaticText*, std::string> static_texts;
+        std::unordered_map<wxChoice*, wxArrayString> choices;
     };
 }
